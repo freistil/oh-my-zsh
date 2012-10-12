f8c8e0f6823476ed94ce64f10d87dbe29c9caadc
Jochen Lillich <jochen@lillich.info>
2012-10-12 14:03:04 +0200

    Fixed quote error in sysadmin.zsh

--- custom/sysadmin.zsh
+++ custom/sysadmin.zsh
@@ -7,7 +7,7 @@ alias chefrun="killall -SIGUSR1 chef-client"
 
 # Show Apache status
 export APWATCH_INTERVAL=3
-alias apwatch="watch -i "\$APWATCH_INTERVAL" 'curl -s -H "Cache-control: no-cache" http://localhost/server-status\?auto'"
+alias apwatch="watch -i "\$APWATCH_INTERVAL" 'curl -s -H \"Cache-control: no-cache\" http://localhost/server-status\?auto'"
 
 # Restart Apache
 alias aprestart="service apache2 restart"
