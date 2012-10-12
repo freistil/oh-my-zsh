93372be643626393b494f6e02540619f78f32ac1
Jochen Lillich <jochen@lillich.info>
2012-10-12 11:46:19 +0200

    More sysadmin helpers

--- custom/aliases.zsh
+++
@@ -1,6 +0,0 @@
-#
-# Aliases
-#
-
-# Wake up a sleeping chef-client
-alias chefrun="killall -SIGUSR1 chef-client"
---
+++ custom/sysadmin.zsh
@@ -0,0 +1,13 @@
+#
+# Sysadmin helpers
+#
+
+# Wake up a sleeping chef-client
+alias chefrun="killall -SIGUSR1 chef-client"
+
+# Show Apache status
+export APWATCH_INTERVAL=3
+alias apwatch="watch -i "\$APWATCH_INTERVAL" 'curl -s -H "Cache-control: no-cache" http://localhost/server-status\?auto'"
+
+# Restart Apache
+alias aprestart="service apache2 restart"
