56ad33d5bec0b373113eaadcf53e4fc336cc5f19
Jochen Lillich <jochen@lillich.info>
2013-03-26 13:27:28 +0100

    Make apwatch alias more flexible by env var

--- custom/sysadmin.zsh
+++ custom/sysadmin.zsh
@@ -7,7 +7,7 @@ alias chefrun="killall -SIGUSR1 chef-client"
 
 # Show Apache status
 export APWATCH_INTERVAL=3
-alias apwatch="watch --interval "\$APWATCH_INTERVAL" 'curl -s -H \"Cache-control: no-cache\" http://localhost/server-status\?auto'"
+alias apwatch="watch --interval "\$APWATCH_INTERVAL" 'curl -s -H \"Cache-control: no-cache\" \${APACHE_STATUSURL:-http://localhost/server-status\?auto}'"
 
 # Restart Apache
 alias aprestart="service apache2 restart"
